85c5a05b84a5f752f0d6ed6e4e2e4bedb2a36193
Mateus C Godoy <mateuscgodoy@gmail.com>
2023-05-07 10:46:14 -0300

    Add Project App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Background from "./Components/Background/Background";
+import SearchResult from "./Components/SearchResults/SearchResults";
+import Playlist from "./Components/Playlist/Playlist";
+import SearchBar from "./Components/SearchBar/SearchBar";
+
+const tracks = [
+  {
+    song: "Rebollation",
+    band: "Red Hot Chilly Peppers",
+    year: "1988",
+    album: "The Number of the Beast",
+  },
+  {
+    song: "Banho de Lua",
+    band: "Angelica",
+    year: "1212",
+    album: "Albom",
+  },
+  {
+    song: "Ciranda-Cirandinha",
+    band: "Palhaço Tragédia",
+    year: "20223",
+    album: "Album Best Golden Increduble Hits",
+  },
+];
 
 function App() {
+  const [selectedTracks, setSelectedTracks] = useState([]);
+
+  function selectTrack(event) {
+    const selectedSong =
+      event.target.parentElement.querySelector(".song-name").innerHTML;
+
+    if (selectedTracks.some((track) => track.song === selectedSong)) {
+      alert("This song is already on your playlist.");
+      return;
+    }
+
+    const trackIndex = tracks.findIndex((track) => track.song === selectedSong);
+    const newSelection = [...selectedTracks, tracks[trackIndex]];
+    setSelectedTracks(newSelection);
+  }
+
+  function removeTrack(event) {
+    const selectedSong =
+      event.target.parentElement.querySelector(".song-name").innerHTML;
+    const updateSelection = selectedTracks.filter(
+      (track) => track.song !== selectedSong
+    );
+    setSelectedTracks(updateSelection);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Background>
+      <section id="main-body">
+        <h1>
+          Jam
+          <br />
+          Time!
+        </h1>
+        <SearchBar />
+        <section className="lists-section">
+          <div>
+            <SearchResult searchResult={tracks} handleClick={selectTrack} />
+          </div>
+          <div>
+            <Playlist tracks={selectedTracks} handleClick={removeTrack} />
+          </div>
+        </section>
+      </section>
+    </Background>
   );
 }
 
